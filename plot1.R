9d768822610af726b89d90ae451a6f2605e06876
Mithun IM <mithun.im@gmail.com>
2015-10-08 21:14:00 +0530

    plot1.R completed

---
+++ plot1.R
@@ -0,0 +1,8 @@
+dat <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+datd <- dat[dat$Date=="2/2/2007" | dat$Date=="1/2/2007",]
+datd_ <- datd[!(datd$Global_active_power == "?"),]
+Global_Active_Power <- as.numeric(as.character(datd_$Global_active_power))
+
+dev.copy(png,file = "plot1.png")
+hist(Global_Active_Power,col = "Red",xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
