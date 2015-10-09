301549d75e368919ffb13fd78024b2d8304c4348
Mithun IM <mithun.im@gmail.com>
2015-10-09 11:10:51 +0530

    plot2.R completed

--- plot2.R
+++ plot2.R
@@ -1,8 +1,10 @@
-dat <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
-datd <- dat[dat$Date=="2/2/2007" | dat$Date=="1/2/2007",]
-datd_ <- datd[!(datd$Global_active_power == "?"),]
+dat                 <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+datd                <- dat[dat$Date=="2/2/2007" | dat$Date=="1/2/2007",]
+atd_                <- datd[!(datd$Global_active_power == "?"),]
 Global_Active_Power <- as.numeric(as.character(datd_$Global_active_power))
-
-dev.copy(png,file = "plot1.png")
-hist(Global_Active_Power,col = "Red",xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+Date                <- as.Date(as.character(datd$Date),format = "%d/%m/%Y")
+Week                <- as.character(weekdays(Date))
+plot(Global_Active_Power,ylab = "Global Active Power (kilowatts)",type = "l",xaxt="n",xlab="")
+axis(1, at=c(1,1441,2881),labels=c("Thu","Fri","Sat"))
+dev.copy(png,file = "plot2.png")
 dev.off()
