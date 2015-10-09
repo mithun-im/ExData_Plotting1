2c2d6649b3e04625c88a70b8940fed40eb1d3b95
Mithun IM <mithun.im@gmail.com>
2015-10-09 16:12:25 +0530

    All programs updated

--- plot3.R
+++ plot3.R
@@ -8,7 +8,7 @@ Sub_Metering2       <- as.numeric(as.character(datd$Sub_metering_2))
 Sub_Metering3       <- as.numeric(as.character(datd$Sub_metering_3))
 Date                <- as.Date(as.character(datd$Date),format = "%d/%m/%Y")
 Week                <- substr(as.character(weekdays(Date)),1,3)
-Week[2881]          <- "Sat"
+Week[2881]          <- substr(weekdays(as.Date("3/2/2007",format="%d/%m/%Y")),1,3)
 png("plot3.png")
 plot(Sub_Metering1,ylab = "Energy sub metering",type = "l",xaxt="n",xlab="")
 lines(Sub_Metering2,ylab = "Energy sub metering",type = "l",xaxt="n",xlab="",col = "Red")
